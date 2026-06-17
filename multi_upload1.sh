b665b66032bcc4a567d670dd8804abf4ca9d326a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-17 20:36:56 +0800

    Update multi_upload1.sh

--- multi_upload1.sh
+++ multi_upload1.sh
@@ -106,8 +106,8 @@ for file_entry in "${FILE_ENTRIES[@]}"; do
     
     # Only show label and links, NO filename anywhere
     DOWNLOADS_SECTION+="
-🔹 ${label} - ${download_links} (${size})"
-🔹 label="🎯 GApps Package" - download_links="<a href=\"https://sourceforge.net/projects/nikgapps/files/Releases/Android-16/\">SourceForge</a>"
+🔹 ${label} - ${download_links} (${size})
+🔹 label="🎯 GApps Package" - download_links="<a href=\"https://sourceforge.net/projects/nikgapps/files/Releases/Android-16/\">SourceForge</a>""
 done 
 
 DOWNLOADS_SECTION+="
