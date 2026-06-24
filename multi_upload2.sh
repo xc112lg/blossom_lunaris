76d97f380cde47e2205658797fdb2298fe0da15f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-24 19:59:22 +0800

    s

--- multi_upload2.sh
+++ multi_upload2.sh
@@ -138,7 +138,7 @@ $DOWNLOADS_SECTION
 <b>📝 Notes:</b>
 • NFC only spawn on angelican on this build
 • Blur effect only work with 3GB memory and up
-• ss
+• Debloat some system apps
 • Signed build
 • Includes MIUI Camera & Lunari Dolby
 • June security patch
