9ee8cba33731f4302a34b61fcfa0c67d50350241
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-24 20:05:33 +0800

    a

--- multi_upload2.sh
+++ multi_upload2.sh
@@ -141,7 +141,6 @@ $DOWNLOADS_SECTION
 ━━━━━━━━━━━━━━━━━━━
 <b>📝 Notes:</b>
 • Isolated NFC issue angelican, so non nfc varaints wont show nfc app
-• Blur effect will only work on 3GB ram up variant let me know if it doesnt work,but it should
 • Signed build
 • Includes MIUI Camera & Lunari Dolby
 • June security patch
