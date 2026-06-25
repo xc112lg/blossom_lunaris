2cc5ebc74ec9778ccb04f4fa626cb9b46e24240f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-25 09:56:17 +0800

    d

--- multi_upload2.sh
+++ multi_upload2.sh
@@ -142,8 +142,7 @@ for file_entry in "${FILE_ENTRIES[@]}"; do
 DOWNLOADS_SECTION+="
 🔹 🎯 GApps Package <a href=\"https://sourceforge.net/projects/nikgapps/files/Releases/Android-16/\">SourceForge</a>"
 done
-DOWNLOADS_SECTION+="
-🔹 🎯 GApps Package <a href=\"https://sourceforge.net/projects/nikgapps/files/Releases/Android-16/\">SourceForge</a>"
+
 DOWNLOADS_SECTION+="
 
 ━━━━━━━━━━━━━━━━━━━
