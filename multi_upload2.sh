df7cb08a96c263bdb81106d987e7f5688b6e5c3e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-25 09:25:13 +0800

    j

--- multi_upload2.sh
+++ multi_upload2.sh
@@ -168,7 +168,7 @@ $DOWNLOADS_SECTION
 <b>📝 Notes:</b>
 • NFC wont spawn on non NFC variant
 • Blur effect will only work for 3GB ram and up variant
-• Signed build
+• Signed
 • Includes MIUI Camera & Lunari Dolby
 • June security patch
 • Default Kernel Sashimi
